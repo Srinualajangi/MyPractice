916fa2752d22725828be47214915d6a053a5ea9b
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-08-21 21:47:08 +0530

    Added roboshop shell script

---
+++ web.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install nginx -y
+VALIDATE $? "Installing nginx"
+systemctl enable nginx &>>LOGFILE
+systemctl start nginx &>>LOGFILE
+rm -rf /usr/share/nginx/html/* &>>LOGFILE
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>LOGFILE
+cd /usr/share/nginx/html 
+unzip /tmp/web.zip &>>LOGFILE
+
