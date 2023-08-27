2d5a08be35651a5a3817671530963f157ffdfa47
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-08-27 10:01:36 +0530

    Modified the script

--- ec2.sh
+++ ec2.sh
@@ -16,8 +16,9 @@ do
     echo "Creating $i Instance "
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS
+
     aws route53 change-resource-record-sets --hosted-zone-id Z0205715205SMVXZPYKDQ --change-batch '
-     {
+    {
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
